0e74ae31962707a81a2c0d9c0df82106add38124
kevin harris <20600822+kcharris@users.noreply.github.com>
2024-08-22 08:40:34 -0500

    Add api test project and unit test for enum method

---
+++ ServerTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using HealthApp.Server;
+
+namespace ServerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual((decimal)1.2, ActivityLevelMethods.GetActivityValue("sedentary"),"GetActiveValue for sedentary did not match expected value");
+            Assert.AreNotEqual((decimal)1.3, ActivityLevelMethods.GetActivityValue("active"), "GetActicityValue for sedentary matched the wrong value");
+        }
+    }
+}
